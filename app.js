43e89b33737eb3847dae40057f659a639d2141ae
Hajiyev2006 <rzah20536@gmail.com>
2024-03-29 19:01:10 +0400

    yeni

---
+++ app.js
@@ -0,0 +1,69 @@
+// // 1. Daxil olunan ədədin sadə yoxsa mürəkkəb olduğunu tapın.
+// let num1 = +prompt('eded: ')
+
+// for(let i = 1; i <= num1; i++){
+//     if(num1 < 2){
+//         console.log('Ne sadedir Ne de murekkeb');
+//         break;
+//     }
+
+//     if((num1 % i === 0 && i !== 1 && i !== num1)){
+//         console.log('Mürəkkəb ədəddir');
+//         break;
+//     }
+
+//     else{
+//         console.log('Sade ededdir');
+//     } 
+// }
+
+
+// // 2. Daxil olunan ədədin mükəmməl olub olmadığını tapın.
+// // let num2 = +prompt('eded')
+// // let sum = 0
+
+// // for(let i = 1; i < num2; i++){
+// //     if(num2 % i === 0){
+// //         sum = sum + i
+// //     }
+// // }
+
+// // if(sum === num2){
+// //     console.log('Mukemmeldi');
+// // } else {
+// //      console.log('Mukemmel deyil');
+// //  }
+
+
+// // 3. Daxil olunan 2 ədəd arasındakı mükəmməl ədədləri göstərin.
+// // let num3 = +prompt('Eded')
+// // let num4 = +prompt('Eded2')
+
+// // for (let eded = num3; eded <= num4; eded++) {
+// //     let sum = 0
+
+// //     for (let i = 1; i < eded; i++) {
+// //         if (eded % i === 0) {
+// //             sum = sum + i
+// //         }
+// //     }
+// //     if (sum === eded) {
+// //         console.log(eded);
+// //     }
+// // }
+//  // 4. Daxil olunan 2 ədəd arasındakı sadə ədədləri göstərin.
+// //  let num5 = +prompt('Eded')
+// //  let num6 = +prompt('Eded2')
+
+// // for(let j =num5; j <= num6; j++){
+// //     for(let i = 1; i<=j; i++){
+// //         if(j % 1 === 0 && i !== 1){
+// //             if (j > i){
+// //                 break;
+// //             }
+// //             if (j === i){
+// //                 console.log(i);
+// //             }
+// //         }
+// //     }
+// // }
